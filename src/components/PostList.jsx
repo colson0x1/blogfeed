14b9d0ddc14ac9d882f68ddb145e52316aecc713
Colson <colsoncalifornication@gmail.com>
2024-03-23 11:18:00 +0545

    wire up connect function from react-redux to post list component + add action creator

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostList extends React.Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
     return <div>Post List</div>;
   }
 }
 
-export default PostList;
+// First argument to connect function is always mapStateToProps
+// We would write null to indicate we don't have any state that we want to get
+// into this component
+// Second argument to connect function is action creator
+
+export default connect(null, { fetchPosts })(PostList);
