6df6ab5f6d645d5f90e6ba217a029e9f2d4eda4f
Colson <colsoncalifornication@gmail.com>
2024-03-23 16:18:49 +0545

    create axios instance + wrong way to create asyn action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,19 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
 // Action creator
-export const fetchPosts = () => {
+// Its looks like correct however it's not correct code
+// This right here, this is a BAD APPROACH!
+// It's bad not because we're using a bad design or that we wrote code incorrectly,
+// It is bad because we're breaking the rules of Redux
+// We're specifically breaking the rules of an Action Creator
+// Using like this, we get an error:
+// : Actions must be a plain object. Instead the actual type was a Promise.
+// : Use custom middleware for async actions.
+export const fetchPosts = async () => {
+  const response = await jsonPlaceholder.get('/posts');
+
   return {
     type: 'FETCH_POSTS',
+    payload: response,
   };
 };
