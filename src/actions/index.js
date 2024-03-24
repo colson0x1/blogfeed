5f8707fb842fe41b5fc05a8e95c4e6858cff5d46
Colson <colsoncalifornication@gmail.com>
2024-03-24 14:26:04 +0545

    hook thunk middleware to redux store + implement async action creator with thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,13 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = () => {
-  // Redux Thunk is going to invoke our function and it passes into it: the
-  // dispatch and getState function as arguments
-  // So with this function that we returned, we're going to receive
-  // dispatch and getState as arguments
-  // So with Redux Thunk we can manually dispatch an action at some point of
-  // time in the future!
-  return function (dispatch, getState) {
-    const promise = jsonPlaceholder.get('/posts');
+// Redux Thunk is going to invoke our function and it passes into it: the
+// dispatch and getState function as arguments
+// So with this function that we returned, we're going to receive
+// dispatch and getState as arguments
+// So with Redux Thunk we can manually dispatch an action at some point of
+// time in the future!
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get('/posts');
 
-    return {
-      type: 'FETCH_POSTS',
-      payload: promise,
-    };
-  };
+  dispatch({ type: 'FETCH_POSTS', payload: response });
 };
