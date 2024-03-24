17b34f701662362d3b7abfebc501f67bdf595062
Colson <colsoncalifornication@gmail.com>
2024-03-24 23:22:14 +0545

    work on post reducer + refactor reducer with switch + add map state to props

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -8,13 +8,21 @@ class PostList extends React.Component {
   }
 
   render() {
+    console.log(this.props.posts);
     return <div>Post List</div>;
   }
 }
+
+// Anytime we want to get some data from Redux side of our application into
+// the React side, we're always going to define the mapStateToProps function
+// and pass it off to connect function
+const mapStateToProps = (state) => {
+  return { posts: state.posts };
+};
 
 // First argument to connect function is always mapStateToProps
 // We would write null to indicate we don't have any state that we want to get
 // into this component
 // Second argument to connect function is action creator
 
-export default connect(null, { fetchPosts })(PostList);
+export default connect(mapStateToProps, { fetchPosts })(PostList);
