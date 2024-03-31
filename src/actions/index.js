d21dfad06278261efbfdf0887d21cae796556884
Colson <colsoncalifornication@gmail.com>
2024-03-31 14:14:50 +0545

    access posts with getState + find unique user ids + remove action creator from user header component

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,10 +10,33 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 // Whenever we call an action creator from inside of an action creator,
 // We need to make sure that we dispatch the result of calling the action creator.
-export const fetchPostsAndUsers = () => async (dispatch) => {
+// Second argument that Redux thunk calls our inner functions with all these
+// action creators, the second argument in addition to dispatch is getState
+// argument.
+// getState is a function that exists on the redux store that gives us access
+// to all of the data inside of Redux
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   console.log('About to fetch posts');
   await dispatch(fetchPosts());
   console.log('fetched posts!');
+  console.log(getState().posts);
+
+  // uniq is going to return an array with just the unique user's ids
+  // _.map is going to get userId from posts array
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  console.log('User Ids', userIds);
+  // For every id, call fetchUser action creator
+  userIds.forEach((id) => dispatch(fetchUser(id)));
+
+  // we don't have any other logic inside of here that we need to run after
+  // we fetch those users.
+  // So we don't need to add await this time on the dispatch
+  // If we had other resources to fetch inside of here then we could use await
+  // keyword up there on dispatch and down here get access to our lists of user
+  // with getState().users, print those out or something like that.
+  // !NOTE! async await keyword doesn't work with forEach
+  // here's how in that case:
+  /* await Promise.all(userIds.map((id) => dispatch(fetchUser(id)))); */
 };
 
 export const fetchPosts = () => async (dispatch) => {
