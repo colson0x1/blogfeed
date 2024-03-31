4ea8f6a0115f8b0ae6c153327e90d8ade3ad3153
Colson <colsoncalifornication@gmail.com>
2024-03-31 13:00:02 +0545

    resolve duplicate request for each individual users with one time Memoization on fetchUser from lodash

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 // Redux Thunk is going to invoke our function and it passes into it: the
@@ -14,8 +15,28 @@ export const fetchPosts = () => async (dispatch) => {
 
 // When we call fetchUser action creator, we want to pass in the id of the
 // user that we want to fetch as an argument
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+
+// Using _ to indicate that this is a private function so to speak and that
+// other Engineers should not attempt to call this function unless they
+// really know what they are doing!
+// This arrow function right here is what actually makes a requests and
+// dispatches an action
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: 'FETCH_USER', payload: response.data });
+});
+
+/* Applying memoize in outer function or inner function
+ * Doesn't work because it gets rememoized here
+export const fetchUser = function (id) {
+  // memoizing interior function
+  // This is the function that gets invoked with Redux Thunk
+  return _.memoize(async function (dispatch) {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+  });
 };
+*/
